f33c6a513335c8e2900dfd1c9df22c00e077c270
Chirag Chopra <chiragshivamccsc@gmail.com>
2025-02-21 01:39:12 +0530

    Fix deprecated errors

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import PyPDF2
-book = open('UPSC Q Paper.pdf', 'rb')    #Write Your Pdf file name in the quotes 
-pdfReader = PyPDF2.PdfFileReader(book)   #To Read a pdf file
-pages = pdfReader.numPages               #To get the number of pages in the pdf
+book = open('Dummy.pdf', 'rb')    #Write Your Pdf file name in the quotes 
+pdfReader = PyPDF2.PdfReader(book)   #To Read a pdf file
+pages = len(pdfReader.pages)              #To get the number of pages in the pdf
 print(pages) 
 speaker = pyttsx3.init()                 #To initialize the pyttsx3
-for num in range(7, pages): 
-    page = pdfReader.getPage(num)        #To get the page to be read
-    text = page.extractText()            #For extraction of text from the page
+for num in range(0, pages): 
+    page = pdfReader.pages[num]        #To get the page to be read
+    text = page.extract_text()            #For extraction of text from the page
     speaker.say(text)
     speaker.runAndWait()
